9ee6fad26ca01665a740c7c874beaf44e7a3922f
JosephineAkello <josephineakello2030@gmail.com>
2023-01-13 00:57:09 +0300

    add examples

--- lib/todos.ex
+++ lib/todos.ex
@@ -1,18 +1,17 @@
 defmodule Todos do
-  @moduledoc """
-  Documentation for `Todos`.
-  """
 
-  @doc """
-  Hello world.
+  def hello_world do
+    # "Hello world"
+    IO.puts("Hellooo world")
+  end
 
-  ## Examples
+  def list do
+    ["bananas", "oranges", "apples"]
+  end
 
-      iex> Todos.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_todos do
+    number_of_tasks = IO.gets("Enter the number of tasks:")
+    {number_of_tasks, _q } = Integer.parse(number_of_tasks)
+    number_of_tasks
   end
 end
